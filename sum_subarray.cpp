206f8512860457b9f2be7f2ec0a1e8240b81b3c7
Ankitabharti24 <110227003+Ankitabharti24@users.noreply.github.com>
2023-08-07 16:26:47 +0530

    Add files via upload

---
+++ sum_subarray.cpp
@@ -0,0 +1,55 @@
+//Given an array arr[] of non-negative integers and an integer sum, 
+//find a subarray that adds to a given sum. There may be more than one subarray 
+//with sum as the given sum, print first such subarray.
+
+
+#include<bits/stdc++.h>
+using namespace std;
+
+void sumofele(int arr[],int n,int k){
+	
+	bool flag=0;
+	int l,m;
+
+	for(int i=0;i<n;i++){
+		int sum=0;
+		for(int j=i;j<n;j++){
+			sum+=arr[j];
+			if(sum==k){
+				l=i;
+				m=j;
+				flag=1;
+				break;
+			}
+			else if(sum>k){
+				break;
+			}			
+		}		
+	}
+	if(flag==1){
+		for(int p=l;p<=m;p++){
+			cout<<arr[p]<<" ";
+		}
+	}
+	else if(flag=0){
+		cout<<"No res";
+	}
+	
+}
+
+int main(){
+	int n,k;
+	
+	cout<<"Enter the size of array: ";
+	cin>>n;
+	
+	int arr[n];
+	cout<<"Enter the elements of array: ";
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	
+	cout<<"The the value of sum: ";
+	cin>>k;
+	sumofele(arr,n,k);
+}
